8d6a5b6e419ed1ef0622b7c8dbbaf0b17ba30dc4
Mike <michal.rehacek@webio.eu>
2025-10-20 13:40:22 +0200

    feat(validators): přidání validace vstupu leadu s kontrolou všech polí

--- lib/validators/index.ts
+++ lib/validators/index.ts
@@ -1,3 +1,7 @@
 
-// Export všech validátorů
-export * from './leadValidator';
+// Validace vstupu pro lead
+import { Lead } from '../types/Lead';
+
+export function isValidLead(lead: Lead): boolean {
+  return Boolean(lead.name && lead.phone && lead.status);
+}
