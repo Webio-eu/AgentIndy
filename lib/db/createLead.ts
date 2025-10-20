312b0b4b5cfd1fa63c6680dd59112c99bd73ac79
Mike <michal.rehacek@webio.eu>
2025-10-20 11:41:40 +0200

    feat(db): vytvoření nového leadu

---
+++ lib/db/createLead.ts
@@ -0,0 +1,14 @@
+// Vtvoreání nové lead
+import { connection } from './connection';
+
+type LeadData = {
+  name: string;
+  phone: string;
+  status: 'standby' | 'done';
+};
+
+export async function createLead(data: LeadData) {
+  const { name, phone, status } = data;
+  const sql = `INSERT INTO leads (name, phone, status) VALUES ('${name}', '${phone}', '${status}')`;
+  await connection.query(sql);
+}
