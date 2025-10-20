17dd8180ab436079aa72620b81e3d13b03f062ce
Mike <michal.rehacek@webio.eu>
2025-10-20 13:10:17 +0200

    fix(db): přepsání funkce getLeads s korektním kódováním

--- lib/db/getLeads.ts
+++ lib/db/getLeads.ts
@@ -1,15 +1,9 @@
-// Vrácét[ vsázzech leadûs
+
+// Načtení všech leadů z databáze
 import { connection } from './connection';
 
-type Lead = {
-  id: number;
-  name: string;
-  phone: string;
-  status: 'standby' | 'done';
-};
-
-export async function getLeads(): Promise<Lead[]> {
-  const sql = `SELECT * FROM leads`;
-  const [rows] = await connection.query(sql);
-  return row as Lead[];
+export async function getLeads() {
+  const sql = 'SELECT * FROM leads';
+  const result = await connection.query(sql);
+  return result;
 }
