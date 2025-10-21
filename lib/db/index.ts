97c0340e9d2a3d0842567101a1ce0974df55bbf3
Mike <michal.rehacek@webio.eu>
2025-10-21 08:23:19 +0200

    feat(db): aktualizace inicializace DB připojení

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,3 +1,6 @@
 
-// Index databázových funkcí
-export * from './leads';
+// Inicializace připojení k databázi
+import mysql from 'mysql2/promise';
+import { DATABASE_URL } from '../env';
+
+export const db = mysql.createPool(DATABASE_URL);
