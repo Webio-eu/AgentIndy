1c58de07673c45e41f6dd2ed2c9852eb9a776e1c
Mike <michal.rehacek@webio.eu>
2025-10-21 08:29:20 +0200

    feat(api): Přidání API endpointu pro výpis všech leadů

---
+++ pages/api/leads.ts
@@ -0,0 +1,9 @@
+
+// API endpoint pro získání všech leadů z databáze
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getLeads } from '../../lib/db/leads';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const leads = await getLeads();
+  res.status(200).json(leads);
+}
