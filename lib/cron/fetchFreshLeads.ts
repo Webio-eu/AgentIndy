11638a1d7f24f874b14c168b509750ba770f28eb
Mike <michal.rehacek@webio.eu>
2025-10-26 09:00:20 +0100

    feat(cron): pravidelné stahování nových leadů

---
+++ lib/cron/fetchFreshLeads.ts
@@ -0,0 +1,9 @@
+// Fetch fresh leads tasks
+// Spousti cron.job uloha
+export async function fetchFreshLeads() {
+  // Http packet, databaze queries, logicka
+  console.log("[CRON] Fetching leads from external sources...");
+
+  // ... dalesi logic
+  return [];
+}
