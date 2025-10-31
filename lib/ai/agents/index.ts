61e19e76bc9e6b10384a9211dd896a1c7e055ea6
Mike <michal.rehacek@webio.eu>
2025-10-31 09:17:02 +0100

    chore(ai): přidány hromadné exporty agentů pro jednodušší správu

---
+++ lib/ai/agents/index.ts
@@ -0,0 +1,4 @@
+export { leadHunter } from './leadHunter';
+export { generateFinancialReport } from './financialReport';
+export { default as callAssistant } from './callAssistant';
+export { default as generateCallTwiml } from './generateCallTwiml';
