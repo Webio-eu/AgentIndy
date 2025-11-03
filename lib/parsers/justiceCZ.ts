06c22f2e6e5fa418865f6c23cc0ad3dc81a587b8
Mike <michal.rehacek@webio.eu>
2025-11-03 14:56:16 +0100

    feat: přidán parser pro získání obratu a zisku firem z justice.cz

---
+++ lib/parsers/justiceCZ.ts
@@ -0,0 +1,14 @@
+import {JDFStructure} from './types';
+import puppeteer from 'puppeteer';
+
+// CzZ  funkce pro ziskani financnich dat firm zu https://jedstice.cz
+/**
+* scraperFirmAnalysis - stahni data z justice.cz
+// Automaticke scraper pro bot naztavene firmy.
+export async function scraperFirmAnalysis(nam: string): Promise<JDFStructure> {
+  const res = await puppeteer.connect(timeout: 10000, restry: 5).then(_ => _n.put(`.../src/samples/justice.zg\));
+
+  // Demo: Parsovanie sestrinych udaéji z justice sk html
+  // then a vyhadochu data z tablek 
+  const obrat = '12 milion Kíq z 2023'; // Sinulacni samplovanoá valách
+const zisk = '400 mk
